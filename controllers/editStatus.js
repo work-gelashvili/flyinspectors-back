c7af0dd591fb07ab4dacd01f9cc393f6f909bfd0
Davit Gelashvili <datoogelashvili@gmail.com>
2024-11-29 17:21:30 +0400

    add router

---
+++ controllers/editStatus.js
@@ -0,0 +1,29 @@
+const ClientModal = require("./../jsonModels/clientModal");
+
+const editStatus = async (req, res) => {
+    try {
+      const { userId, status } = req.body;
+      // const { userId, status } = req.query;
+      
+      const clients = await ClientModal.find();
+      
+      
+      const filtered = clients.filter((item) => item.userId === userId)
+      console.log(filtered)
+
+      await ClientModal.findOneAndUpdate(
+        { userId: userId },
+        { status: status }
+      );
+    
+    
+      // const send = await clients.save();
+    
+      // return res.status(200).send(send);
+      return res.status(200).send("Car seat has been reserved!");
+    } catch (error) {
+        return res.status(500).send("Something went wrong while creating car!");
+    }
+};
+
+module.exports = {editStatus}
--- router.js
+++ router.js
@@ -1,4 +1,5 @@
-const {createClient} = require('./controllers/createClient')
+const {createClient} = require('./controllers/createClient');
+const { editStatus } = require('./controllers/editStatus');
 const {emailSend} = require('./controllers/email');
 const { getClient } = require('./controllers/getClient');
 const { getID } = require('./controllers/getID');
@@ -13,5 +14,6 @@ router.post("/client", createClient);
 router.post("/email", emailSend);
 router.get("/client", getClient);
 router.post("/id", getID);
+router.put("/client/id", editStatus);
 
 module.exports = router;
