27589007077c31df1c9b79f8665c02ed86f0d50c
Davit Gelashvili <datoogelashvili@gmail.com>
2025-01-22 23:15:13 +0400

    fix

--- controllers/getClient.js
+++ controllers/getClient.js
@@ -2,15 +2,22 @@ const ClientModal = require("./../jsonModels/clientModal");
 
 const getClient = async (req, res) => {
     try {
-      
-  
       const clients = await ClientModal.find();
-  
-      return res.status(200).send(clients);
+
+      // Reverse the order of clients
+      const reversedClients = clients.reverse();
+
+      // Map over the reversed clients and transform the data if needed
+      const result = reversedClients.slice(0, 20).map(client => {
+          return client
+      });
+
+      // Return the transformed and limited client list
+      return res.status(200).send(result);
     } catch (error) {
-      console.log(error)
-      return res.status(500).send("Something went wrong while getting cars!");
+        console.log(error);
+        return res.status(500).send("Something went wrong while getting clients!");
     }
 };
 
-module.exports = {getClient}
+module.exports = { getClient };
